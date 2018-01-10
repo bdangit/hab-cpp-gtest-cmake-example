9fabfc325e7c3c6f3950023ecf46f7d772bbd08c
Ben Dang <me@bdang.it>
2018-01-09 18:25:16 -0800

    successful habitat build
    Signed-off-by: Ben Dang <me@bdang.it>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -6,12 +6,16 @@ pkg_build_deps=(
   core/gcc
   core/cmake
   core/make
-  core/openssl
+  core/pkg-config
+  bdangit/googletest
+  bdangit/googlemock
 )
 pkg_deps=(
   core/glibc
   core/gcc-libs
 )
+
+pkg_bin_dirs=(bin)
 
 BUILDDIR='build'
 
@@ -25,10 +29,19 @@ do_build() {
     -B${BUILDDIR} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-  make -C "${BUILDDIR}"
+  make -C "${BUILDDIR}" VERBOSE=${DEBUG}
 }
 
 do_check() {
+  # this export is to make `testfoo` binary happy
+  # need to figure out how to maniuplate RPATH of `testfoo` to include googletest and googlemock
+  # at buildtime.  Either set $ENV{LD_RUN_PATH} or use `core/pathelf` to modify the rpath
+  # export LD_LIBRARY_PATH
+  # LD_LIBRARY_PATH="$(pkg_path_for bdangit/googletest)/lib;$(pkg_path_for bdangit/googlemock)/lib"
+  # LD_LIBRARY_PATH="$(pkg_path_for bdangit/googlemock)/lib"
+  #
+  # build_line "Plan Setting LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+
   make -C "${BUILDDIR}" test
 }
 
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,11 @@
 #include "bar.h"
 #include "foo.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
+    printf("Woof Woof!\n");
+    
     Bar bar;
     Foo foo(bar);
     foo.baz(true);
